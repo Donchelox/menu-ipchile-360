8c17f9e0f1ba1ac1861756765a9f89d9cb1c4af4
Donchelox <inside.4.me@gmail.com>
2020-09-13 12:38:51 -0300

    separacion archivos

---
+++ app.js
@@ -0,0 +1,18 @@
+function toggleSidebar(){
+    document.getElementById("scroll-menu").classList.toggle('stgo-active');
+}
+function toggleSidebarRancagua(){
+    document.getElementById("scroll-menu").classList.toggle('rancagua-active');
+}
+function toggleSidebarLaSerena(){
+    document.getElementById("scroll-menu").classList.toggle('laserena-active');
+}
+function toggleSidebarTemuco(){
+    document.getElementById("scroll-menu").classList.toggle('temuco-active');
+}
+function toggleSiderbarMenu(){
+    document.getElementById("sidebar-menu").classList.toggle('close');
+}
+function toggleCloseSiderbar(){
+    document.getElementById("close").classList.toggle('d-none');
+}
